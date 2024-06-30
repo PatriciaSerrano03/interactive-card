d851d0b92b232addd4fa6ba05f28e5ba5903f39c
PatriiSerrano <patricia.serranomolina@gmail.com>
2024-06-30 19:03:54 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,29 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElement = document.getElementById('form');
+const errorElement = document.querySelectorAll('.error');
+const inputsElement = document.querySelectorAll('input[type=text]');
+
+const printError = index => {
+  errorElement[index - 1].textContent = 'VACIO';
+};
+
+const hideError = index => {
+  errorElement[index].textContent = '';
+};
+
+const validateForm = () => {
+  inputsElement.forEach((input, index) => {
+    if (input.value === '') {
+      index > 2 ? printError(index - 1) : printError(index);
+    } else hideError(index);
+  });
+};
+
+console.log(validateForm);
+
+formElement.addEventListener('submit', event => {
+  event.preventDefault();
+  validateForm();
+});
